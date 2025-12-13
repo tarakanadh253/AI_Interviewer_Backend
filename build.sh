a4cef7d2d4519cb1dc262e655da7c31f9fdad00b
nvtarakanadh <nvtarakanadh@gmail.com>
2025-12-13 21:01:48 +0530

    Install CPU-only PyTorch to reduce slug size and build time on Render

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 set -o errexit
 
 python -m pip install --upgrade pip
+# Install CPU-only torch first to avoid downloading 800MB+ CUDA version
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cpu
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
